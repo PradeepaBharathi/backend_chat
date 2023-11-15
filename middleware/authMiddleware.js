7549bb68bb28006e73da8d845d1a8932cee3bcc2
PradeepaBharathi <bharathi83201@gmail.com>
2023-11-15 17:22:25 +0530

    removed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../modals/userModel.js");
-const asyncHandler = require("express-async-handler.js");
+const asyncHandler = require("express-async-handler");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
