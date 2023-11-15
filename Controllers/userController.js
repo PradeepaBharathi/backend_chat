4c92020ef700f3667627795fafb2e1e99daac61c
PradeepaBharathi <bharathi83201@gmail.com>
2023-11-15 17:19:41 +0530

    updates

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,6 @@
 const generateToken = require("../Config/generateToken");
 const UserModel = require("../modals/userModel.js");
-const expressAsyncHandler = require("express-async-handler.js");
+const expressAsyncHandler = require("express-async-handler");
 // Login
 const loginController = expressAsyncHandler(async (req, res) => {
   console.log(req.body);
