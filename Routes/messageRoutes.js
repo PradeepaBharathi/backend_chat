1cd710ffb3f3d5a1fffe3e275e50b68443b76653
PradeepaBharathi <bharathi83201@gmail.com>
2023-11-15 17:52:45 +0530

    working

--- Routes/messageRoutes.js
+++ Routes/messageRoutes.js
@@ -1,9 +1,7 @@
 import  express from "express";
-import  {
-  allMessages,
-  sendMessage,
-} from "../controllers/messageControllers.js";
+
 import { protect } from "../middleware/authMiddleware.js";
+import { allMessages, sendMessage } from "../controllers/messageControllers.js";
 
 const router = express.Router();
 
